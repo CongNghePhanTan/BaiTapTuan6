8d5b623b4437442232416d6678ab32567e60ee92
Lê Cao Diễm Quyên <diemquyen2596dhcn1b@gmail.com>
2017-10-17 00:13:32 +0700

    Update MaxMinClientComponent.java

--- de4/MaxMinClientComponent.java
+++ de4/MaxMinClientComponent.java
@@ -1,32 +1,43 @@
+import java.net.MalformedURLException;
 import java.rmi.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class MaxMinClientComponent 
-{
-    private static final String host = "localhost";
-
-    public static void main(String[] args) 
-    {
-        try 
-        {
-            //We obtain a reference to the object from the registry and next,
-            //it will be typecasted into the most appropiate type.
-            MaxMin greeting_message = (MaxMin) Naming.lookup("rmi://" 
-                    + host + "/MaxMin");
-
-            //Next, we will use the above reference to invoke the remote
-            //object method.
-            System.out.println("Message received: " + 
-                    greeting_message.getGreetingMessage(args));
-        } 
-        catch (ConnectException conEx) 
-        {
-            System.out.println("Unable to connect to server!");
-            System.exit(1);
-        } 
-        catch (Exception ex) 
-        {
-            ex.printStackTrace();
-            System.exit(1);
+public class MaxMinClientComponent {
+  
+    public static void main(String[] args){
+        new MaxMinClientComponent().run();
+    }
+    private void run(){
+     
+        int n;
+        int a[];
+        try {
+           IMaxMin maxmin=(IMaxMin) Naming.lookup("rmi://localhost:"+2012+"/MaxMinImplementation");
+            System.out.println("==== Nhap so phan tu cua mang  ====");
+            System.out.println("-Moi nhap: ");
+            Scanner scan =new Scanner(System.in);
+            n=scan.nextInt();
+          a = new int[n];
+             for (int i=0; i<a.length;i++)
+                {
+                    System.out.print("Nhap phan tu thu"+(i+1)+": ");
+                    a[i] = scan.nextInt();
+                }
+             int b[]=new int[a.length];
+             b=maxmin.SapXep(a);
+       System.out.println("Max=");
+       System.out.println(b[b.length-1]+"");
+        System.out.println("Min=");
+        System.out.println(b[0]+"");
+       
+        } catch (NotBoundException ex) {
+            Logger.getLogger(MaxMinClientComponent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(MaxMinClientComponent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(MaxMinClientComponent.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
